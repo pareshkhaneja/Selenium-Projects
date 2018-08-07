fd5b229336ba02e29f8182934c692b0856f9f917
Paresh Khaneja <pareshkhaneja@gmail.com>
2018-08-07 12:27:54 +0530

    Update get_website.py

--- Practice/get_website.py
+++ Practice/get_website.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver=webdriver.Chrome()
-
-driver.get("https://learn.letskodeit.com/p/practice")
-if "Practice | Let's Kode It" in driver.title:
-    print("Website successfully opened.")
-else:
-    print("Website couldn't get opened.")
-
-driver.close()
+class ChromeTest():
+    
+    def get():
+        #Instantiate chrome browser 
+        driver=webdriver.Chrome()
+        
+        #Open provided URL
+        driver.get("https://learn.letskodeit.com/p/practice")
+        if "Practice | Let's Kode It" in driver.title:
+            print("Website successfully opened.")
+        else:
+            print("Website couldn't get opened.")
+            driver.close()
+            
+chrome = ChromeTest()
+chrome.get()
+    
